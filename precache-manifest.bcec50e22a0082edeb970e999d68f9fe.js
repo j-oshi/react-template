9341bbc8fdd2d6eb06718966c80f85d00ec8c0e8
jayosh65 <jtoshin@gmail.com>
2020-04-15 09:22:03 +0100

    Updates

---
+++ precache-manifest.bcec50e22a0082edeb970e999d68f9fe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4e4499fa5b2ac57704f7739cc4784e",
+    "url": "/react-template/index.html"
+  },
+  {
+    "revision": "16a6acc24884c7624b54",
+    "url": "/react-template/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "018e74da4e3f6771ea6a",
+    "url": "/react-template/static/js/10.90cfcdff.chunk.js"
+  },
+  {
+    "revision": "85d8d4b8c848ac3385be",
+    "url": "/react-template/static/js/11.86695b39.chunk.js"
+  },
+  {
+    "revision": "4cd0692e0641aabbb240",
+    "url": "/react-template/static/js/2.9fcfd5db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-template/static/js/2.9fcfd5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a79d54976d2370e05a",
+    "url": "/react-template/static/js/3.884f9ee0.chunk.js"
+  },
+  {
+    "revision": "62f6e5224fa653d69554",
+    "url": "/react-template/static/js/4.285a91fe.chunk.js"
+  },
+  {
+    "revision": "f93ece387d3359c48a9c",
+    "url": "/react-template/static/js/5.7411368e.chunk.js"
+  },
+  {
+    "revision": "42fa012139d4005e83c5",
+    "url": "/react-template/static/js/6.f9cdd655.chunk.js"
+  },
+  {
+    "revision": "f3a939eb6466a5e85172",
+    "url": "/react-template/static/js/7.4de189a8.chunk.js"
+  },
+  {
+    "revision": "bbd5de5477597e600a4c",
+    "url": "/react-template/static/js/8.048f37b1.chunk.js"
+  },
+  {
+    "revision": "4f0465c167435c92f339",
+    "url": "/react-template/static/js/9.1fc6aa7e.chunk.js"
+  },
+  {
+    "revision": "16a6acc24884c7624b54",
+    "url": "/react-template/static/js/main.31dfd667.chunk.js"
+  },
+  {
+    "revision": "6340510c085c56dd338c",
+    "url": "/react-template/static/js/runtime-main.d310ffb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
